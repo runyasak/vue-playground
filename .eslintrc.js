1ff9298862ac809e63d7ab3c8bc3c2e175c51bbc
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-10-04 01:58:51 +0700

    chore: set up linter with rules

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'airbnb-base'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'import/no-unresolved': 'off',
+    'space-before-function-paren': 'off',
+    'object-curly-newline': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'state',
+        'el',
+        'Vue'
+      ]
+    }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
