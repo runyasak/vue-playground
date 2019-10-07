73977b7a577430dcac8b8365fb490382306a58e4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-10-07 18:26:55 +0700

    feat: add router history mode and other path redirect to HelloWorld

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/hello-world',
@@ -17,6 +18,10 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '*',
+      redirect: { name: 'HelloWorld' }
     }
   ]
 })
