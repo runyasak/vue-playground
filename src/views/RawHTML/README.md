58080bc9386b07ca73db903706f4c719ea9ef034
Paretkf <paretk7@gmail.com>
2019-10-16 18:23:51 +0700

    เพิ่ม raw html

---
+++ src/views/RawHTML/README.md
@@ -0,0 +1,6 @@
+# RawHTML
+
+Raw HTML คือ การนำ Plain text ที่เป็น HTML ออกมาแสดง เหมือนเขียน HTML ในเว็บโดยตรง
+
+จากตัวอย่าง จะแสดงค่า `This should be red.` ให้ตัวอักษรเป็นสีแดง
+โดยการใช้ `v-html` directive ตามตัวอย่างนี้
---
+++ src/views/RawHTML/code.js
@@ -0,0 +1,13 @@
+export default `<template>
+  <span v-html="rawHtml"></span>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      rawHtml: '<span style="color: red">This should be red.</span>'
+    }
+  }
+}
+<\/script>
+`
